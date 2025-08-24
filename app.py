36d0f4ab30238b01222cc0904eb24776d93f8fb3
RACUEVA <ra.cuevt@uea.edu.ec>
2025-08-24 13:50:17 -0500

    semana 10

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "¡mi_proyecto_flask!"
+    #return "¡mi_proyecto_flask!"
+    return render_template('index.html')
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
     return f'Bienvenido, {nombre}!'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contacto')
 def contacto():
-    return "bienvenido a la pagina de contactos"
+    #return "bienvenido a la pagina de contactos"
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
